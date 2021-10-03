d00fb10b1514ec32ead27d782b634e5ee231ac68
Peter Frost <frosticless@hotmail.com>
2021-10-04 00:21:19 +0100

    Update webp package

--- webp_compile.sh
+++ webp_compile.sh
@@ -3,20 +3,21 @@
 OS_PACKAGE_MANAGER=$1
 OS_PACKAGE_MANAGER_FLAGS=$2
 
-LESS_PIPE="BEGIN { ORS=\"\\r\"; print \"Starting...\" } { print \"\033[2K\"; print \$0 } END { print \"\033[2K\"; print \"Done\\n\" }"
 
-$OS_PACKAGE_MANAGER install $OS_PACKAGE_MANAGER_FLAGS libjpeg-dev libpng-dev libtiff-dev libgif-dev | awk "$LESS_PIPE"
+$OS_PACKAGE_MANAGER install $OS_PACKAGE_MANAGER_FLAGS libjpeg-dev libpng-dev libtiff-dev libgif-dev
 
 mkdir -p ~/libwebp_sources
 cd ~/libwebp_sources
 
-wget -q https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.1.0.tar.gz
-tar xzvf libwebp-1.1.0.tar.gz | awk "$LESS_PIPE"
+wget -q https://github.com/webmproject/libwebp/archive/refs/tags/v1.2.1.tar.gz
+tar xzvf v1.2.1.tar.gz
+rm -f v1.2.1.tar.gz
 
-cd libwebp-1.1.0
-./configure --enable-shared=false | awk "$LESS_PIPE"
-make -j$(grep -c ^processor /proc/cpuinfo) 2>&1 | awk "$LESS_PIPE"
-sudo make install | awk "$LESS_PIPE"
+cd libwebp-1.2.1
+./autogen.sh
+./configure --enable-shared=false
+make -j$(grep -c ^processor /proc/cpuinfo) 2>&1
+sudo make install
 
 hash -r
 source ~/.profile
